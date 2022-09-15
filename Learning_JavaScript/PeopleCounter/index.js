ca0cf6812218f45bbc9373a684fe33a1690cea16
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-15 10:04:41 -0700

    added previous counts to main page, save now updates the pervious counts/

--- Learning_JavaScript/PeopleCounter/index.js
+++ Learning_JavaScript/PeopleCounter/index.js
@@ -4,6 +4,9 @@ let count = 0
 // get the HTML element for the counter that we want to increment
 let countEl = document.getElementById("count-el")
 
+// get the HTML element for the previous counts
+let prevCounts = document.getElementById("previous-counts")
+
 // listen for clicks on the increment button
 function increment() {
     // increment the count variable when the button is clicked
@@ -13,4 +16,14 @@ function increment() {
     countEl.innerText = count
 }
 
+// save function for the save button to record number of people
+function save() {
+    // update the previous-count element
+    prevCounts.innerText =  prevCounts.innerText + " - " + count.toString()
+    console.log(prevCounts.innerText)
+
+    // reset the main count back to 0
+    count = 0
+    countEl.innerText = count
+}
 
