a880af2f272c9878a9d04360557d9a2c9eed8579
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-19 14:55:39 -0700

    added dealers cards, first card show to player till player busts then second card and total revealed to player.

--- Learning_JavaScript/BlackJackGame/index.js
+++ Learning_JavaScript/BlackJackGame/index.js
@@ -7,6 +7,9 @@ let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.querySelector("#sum-el")
 let cards = []
+let dealerCards = []
+let dealerSumEl = document.getElementById("dealer-sum-el")
+let dealerCardsEl = document.getElementById("dealer-cards-el")
 
 let player = {
     name: "Andrew",
@@ -18,10 +21,24 @@ let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    return Math.floor(Math.random()*10)+2
+    let randomNumber = Math.floor(Math.random()*13)+1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
+    dealerSum = 0
+    let dealerFirstCard = getRandomCard()
+    let dealerSecondCard = getRandomCard()
+    dealerCards = [dealerFirstCard, dealerSecondCard]
+    dealerSum = dealerCards[0] + dealerCards[1]
+
+    hasBlackJack = false
     isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
@@ -29,6 +46,11 @@ function startGame() {
     sum = firstCard + secondCard
 
     renderGame()
+    dealerSetup()
+}
+
+function dealerSetup() {
+    dealerCardsEl.textContent = "Dealers Cards: " + dealerCards[0]
 }
 
 function renderGame() {
@@ -49,6 +71,12 @@ function renderGame() {
     } else {
         message = "Sorry, busted you lose."
         isAlive = false
+        dealerCardsEl.textContent = "Dealers Cards: "
+        for ( let i=0; i<dealerCards.length; i++) {
+            dealerCardsEl.textContent += cards[i] + " "
+        }
+
+        dealerSumEl.textContent = "Dealers Total: " + dealerSum
     }
 
     messageEl.textContent = message
@@ -62,5 +90,3 @@ function hit() {
         renderGame()
     }
 }
-    
-
