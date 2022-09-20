5090ff0ad622e8bf605c8259e650c32be1e2b0b3
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-20 11:01:54 -0700

    added array to hold addresses, get value from input when button is clicked and add value to addresses array.

--- Learning_JavaScript/ChromeExtension/index.js
+++ Learning_JavaScript/ChromeExtension/index.js
@@ -1,5 +1,8 @@
-let inputBtn = document.getElementById("input-btn")
+let myAddresses = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
 
 inputBtn.addEventListener("click", function () {
-    console.log("clicked from addEventListener")
+    myAddresses.push(inputEl.value)
+    console.log(myAddresses)
 })
