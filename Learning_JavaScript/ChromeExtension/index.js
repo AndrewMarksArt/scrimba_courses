916c6265927cff8b038cf1455c61591bb29ead59
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-20 14:45:53 -0700

    updated the list items using a template string to list the input values as links and open them in a new tab when clicked

--- Learning_JavaScript/ChromeExtension/index.js
+++ Learning_JavaScript/ChromeExtension/index.js
@@ -5,13 +5,20 @@ const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myAddresses.push(inputEl.value)
+    inputEl.value = ''
     renderAddresses()
 })
 
 function renderAddresses() {
     let listItems = ""
     for (let i=0; i<myAddresses.length; i++) {
-        listItems += "<li>" + myAddresses[i] + "</li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myAddresses[i]}'>
+                    ${myAddresses[i]}
+                </a>
+            </li>
+        `
     }
     ulEL.innerHTML = listItems
 }
