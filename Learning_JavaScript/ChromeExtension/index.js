e6c051b04cc1e8efaa00a6021f374ff709f37801
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-20 14:30:40 -0700

    updated so now text value from input is saved and displayed in a list each time the save input button is clicked.

--- Learning_JavaScript/ChromeExtension/index.js
+++ Learning_JavaScript/ChromeExtension/index.js
@@ -1,12 +1,17 @@
-let myAddresses = ["test", "test", "test"]
+let myAddresses = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myAddresses.push(inputEl.value)
+    renderAddresses()
 })
 
-for (let i=0; i<myAddresses.length; i++) {
-    ulEL.innerHTML += "<li>" + myAddresses[i] + "</li>"
+function renderAddresses() {
+    let listItems = ""
+    for (let i=0; i<myAddresses.length; i++) {
+        listItems += "<li>" + myAddresses[i] + "</li>"
+    }
+    ulEL.innerHTML = listItems
 }
