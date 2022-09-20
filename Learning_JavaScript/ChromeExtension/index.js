bf78592a8fc8c58973c5774910d8a31055b4734a
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-20 12:01:50 -0700

    added unordered list to html, renders list elements from myAddress array, using temp placeholder text for now.

--- Learning_JavaScript/ChromeExtension/index.js
+++ Learning_JavaScript/ChromeExtension/index.js
@@ -1,8 +1,12 @@
-let myAddresses = []
+let myAddresses = ["test", "test", "test"]
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myAddresses.push(inputEl.value)
-    console.log(myAddresses)
 })
+
+for (let i=0; i<myAddresses.length; i++) {
+    ulEL.innerHTML += "<li>" + myAddresses[i] + "</li>"
+}
