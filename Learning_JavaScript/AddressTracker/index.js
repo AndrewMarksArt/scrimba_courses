f25cddb8c71ac351289324ec8d3067546f6daa48
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-21 13:30:29 -0700

    renamed project folder, adding localStrage to save addresses we input.

--- Learning_JavaScript/AddressTracker/index.js
+++ Learning_JavaScript/AddressTracker/index.js
@@ -1,4 +1,5 @@
 let myAddresses = []
+
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEL = document.getElementById("ul-el")
@@ -6,7 +7,12 @@ const ulEL = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function () {
     myAddresses.push(inputEl.value)
     inputEl.value = ''
+
+    localStorage.setItem("myAdresses", JSON.stringify(myAddresses))
+
     renderAddresses()
+
+    console.log(localStorage.getItem("myAddresses"))
 })
 
 function renderAddresses() {
