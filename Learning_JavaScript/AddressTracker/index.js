8b433468c3fa801b53ee3b96669f512b08702505
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-22 07:43:23 -0700

    check if there is anything saved in localStorage, if there is render out list of items if not log out nothing here.

--- Learning_JavaScript/AddressTracker/index.js
+++ Learning_JavaScript/AddressTracker/index.js
@@ -5,7 +5,15 @@ const inputEl = document.getElementById("input-el")
 const ulEL = document.getElementById("ul-el")
 
 let addressesFromLocal = JSON.parse(localStorage.getItem("myAddresses"))
-console.log(addressesFromLocal)
+// check if localStorage has anything
+// if there is localStorage, set myAddresses to what is there then render out
+if (addressesFromLocal) {
+    console.log("there are addresses saved in local storage")
+    myAddresses = addressesFromLocal
+    renderAddresses()
+} else {
+    console.log("nothing here")
+}
 
 inputBtn.addEventListener("click", function () {
     myAddresses.push(inputEl.value)
