36f8cc359f8b51c3dbfd72402dc7ef291fa80190
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-28 11:54:23 -0700

    setting up react and looking at how we can use ReactDOM.render() to render html from the js file.

---
+++ Learning_React/First_React_Site/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+console.log(page)
+
+ReactDOM.render(page, document.getElementById("root"))
+
+
