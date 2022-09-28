79c8a757940fe5a82660f9f5079d7474138ddb94
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-28 16:23:44 -0700

    seperated out components to individual js files, import them into index.js

--- Learning_React/First_React_Site/index.js
+++ Learning_React/First_React_Site/index.js
@@ -1,41 +1,10 @@
 // import React from "react"
 // import ReactDOM from "react-dom/client"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 
-function Header() {
-    return (
-        <header>
-            <nav>
-                <img src="./icon.jpg" width="40px" />
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>FunFacts about React</h1>
-
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </div>
-    )
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>@ 2022 Marks development. all rights reserved</small>
-        </footer>
-    )
-}
-
-function Page() {
+function App() {
     return (
         <div>
             <Header />
@@ -46,4 +15,4 @@ function Page() {
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Page />)
+root.render(<App />)
