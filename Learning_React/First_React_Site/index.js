69094aebf7b1c386b620ad00d03d1cbaf8635947
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-28 15:31:07 -0700

    creating component and render it to page

--- Learning_React/First_React_Site/index.js
+++ Learning_React/First_React_Site/index.js
@@ -1,14 +1,31 @@
-import React from "react"
-import ReactDOM from "react-dom"
+// import React from "react"
+// import ReactDOM from "react-dom/client"
 
-const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
-    </div>
-)
-console.log(page)
+function PageComponent () {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src="./icon.jpg" width="40px" />
+                </nav>
+            </header>
+            
+            <h1>FunFacts about React</h1>
+            
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
 
-ReactDOM.render(page, document.getElementById("root"))
+            <footer>
+                <small>@ 2022 Marks development. all rights reserved</small>
+            </footer>
+        </div>
+    )
+}
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<PageComponent />)
