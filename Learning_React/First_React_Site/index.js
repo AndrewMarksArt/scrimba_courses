d8d90ab0c80e26df13c7544d56491cf39b92b8cb
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-28 16:37:07 -0700

    set up local react app with vite, updated gitignore, added Battle Warriors logo as Icon to make sure everything was working.

--- Learning_React/First_React_Site/index.js
+++ Learning_React/First_React_Site/index.js
@@ -1,5 +1,5 @@
-// import React from "react"
-// import ReactDOM from "react-dom/client"
+import React from "react"
+import ReactDOM from "react-dom/client"
 import Header from "./Header"
 import MainContent from "./MainContent"
 import Footer from "./Footer"
