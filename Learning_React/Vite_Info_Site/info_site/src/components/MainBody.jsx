ef637c0ecc72c3d7f6b3a0c7b44cac6eafb29d17
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-04 11:31:12 -0700

    added main body title, unordered list, and background image. Updated all css styles to match figma design.

--- Learning_React/Vite_Info_Site/info_site/src/components/MainBody.jsx
+++ Learning_React/Vite_Info_Site/info_site/src/components/MainBody.jsx
@@ -3,9 +3,19 @@ import "../css/main.css"
 export default function MainBody() {
     return (
       <>
-        <div className="main_body">
-          <h1>Main Body Components</h1>
-        </div>
+        <main>
+
+          <h1 className="main--title">Fun facts about React</h1>
+
+          <ul className="main--facts">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+
+        </main>
       </>
     );
   }
