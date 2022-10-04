b592c357fd7592615a63c054b5676260f53a4205
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-04 10:50:10 -0700

    set up nav bar and basic styling

--- Learning_React/Vite_Info_Site/info_site/src/components/NavBar.jsx
+++ Learning_React/Vite_Info_Site/info_site/src/components/NavBar.jsx
@@ -2,10 +2,10 @@ import "../css/main.css"
 
 export default function Welcome() {
     return (
-      <>
-        <div className="navbar">
-          <h1>NavBar Components</h1>
-        </div>
-      </>
+      <nav>
+        <img src="../images/react-icon.png"className="nav--icon"/>
+        <h3 className="nav--logo_text">React Facts</h3>
+        <h4 className="nav--title">React Course -Project 1</h4>
+      </nav>
     );
   }
