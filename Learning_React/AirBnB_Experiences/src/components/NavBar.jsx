d2ce4d8029621319bd56316cf18dda5d13f3fe5b
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-05 15:48:43 -0700

    deleted unneeded files created at setup, created nav and hero components, updated styles, added images from Figma file provided.

---
+++ Learning_React/AirBnB_Experiences/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "../css/main.css"
+
+export default function NavBar() {
+    return (
+        <>
+            <nav>
+                <img src="../src/assets/nav_logo.png" className="nav--logo"/>
+            </nav>
+        </>
+    )
+}
