38dc4ae8da270e4e6ab2713f668f83dd9b0fd25e
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-05 16:16:24 -0700

    added card components and started creating the fist one by hard coding values to test styling. need to update and make more dynamic.

--- Learning_React/AirBnB_Experiences/src/App.jsx
+++ Learning_React/AirBnB_Experiences/src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
+import Card from './components/Card'
 
 export default function App() {
     return (
         <>
             <NavBar />
             <Hero />
+            <Card />
         </>
     )
 }
---
+++ Learning_React/AirBnB_Experiences/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import "../css/main.css"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img src="../src/assets/katie_zaferes_card.png" className="card--image"/>
+            <div className="card--stats">
+                <img src="../src/assets/star.png" className="card--star"/>
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+}
