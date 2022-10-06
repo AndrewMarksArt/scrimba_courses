da004778fa70c25fc2962a48a97fbe6dc51b08a5
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-06 10:27:08 -0700

    removed hardcoded data and placed card details in a dat file, use array maapping to format data for the card component and render the experience cards

--- Learning_React/AirBnB_Experiences/src/App.jsx
+++ Learning_React/AirBnB_Experiences/src/App.jsx
@@ -1,30 +1,26 @@
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 import Card from './components/Card'
+import CardData from './components/CardData'
 
 
 export default function App() {
+    const cardElements = CardData.map(card => {
+        return <Card 
+                    coverImg={card.coverImg}
+                    rating={card.rating}
+                    reviewCount={card.reviewCount}
+                    location={card.location}
+                    title={card.title}
+                    price={card.price}
+                />
+    })
     return (
         <>
             <NavBar />
             <Hero />
-            
-            <Card 
-                img="./src/assets/katie_zaferes_card.png"
-                rating="5.0"
-                num_reviews="(6)"
-                country="USA"
-                title="Life lessons with Katie Zaferes"
-                price="$136"
-            />
-            <Card 
-                img="./src/assets/wedding_photography.png"
-                rating="5.0"
-                num_reviews="(30)"
-                country="USA"
-                title="Learn wedding photography"
-                price="$125"
-            />
+
+            {cardElements}
         </>
     )
 }
--- Learning_React/AirBnB_Experiences/src/components/Card.jsx
+++ Learning_React/AirBnB_Experiences/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import "../css/main.css"
 
-export default function Card({img, rating, num_reviews, country, title, price}) {
+export default function Card({coverImg, rating, reviewCount, location, title, price}) {
 
     return (
         <div className="card">
-            <img src={img} className="card--image"/>
+            <img src={`../src/assets/${coverImg}`} className="card--image"/>
             <div className="card--stats">
                 <img src="../src/assets/star.png" className="card--star"/>
                 <span>{rating}</span>
-                <span className="gray">{num_reviews} • </span>
-                <span className="gray">{country}</span>
+                <span className="gray">({reviewCount}) • </span>
+                <span className="gray">{location}</span>
             </div>
             <p className="card--title">{title}</p>
-            <p><span className="bold">From {price}</span> / person</p>
+            <p><span className="bold">From ${price}</span> / person</p>
         </div>
     )
 }
