8e6ac96a70ada561cdc02cb53d24394981a1e5d0
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-07 15:50:35 -0700

    finished the Header component, added troll face logo, title, and project number with background gradient.

--- Learning_React/Meme_Generator/src/components/Header.jsx
+++ Learning_React/Meme_Generator/src/components/Header.jsx
@@ -2,8 +2,10 @@ import "../css/main.css"
 
 export default function Header() {
     return (
-        <div>
-            <h1>This is the header</h1>
+        <div className="header">
+            <img src="./src/assets/troll_face.png" className="header--logo" />
+            <h3 className="header--title">MEME GENERATOR</h3>
+            <p className="header--project">React Course -- Project 3</p>
         </div>
     )
 }
---
+++ Learning_React/Meme_Generator/src/components/Meme.jsx
@@ -0,0 +1,9 @@
+import "./css/main.css"
+
+export default function Meme() {
+    return (
+        <>
+            <h1>This is where the form and button go</h1>
+        </>
+    )
+}
