76aac241cd1c43ce491fc50181ded0194696d678
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-07 11:32:11 -0700

    added location data for each trip in the travel journal that the Card component will use to render the different trips.

---
+++ Learning_React/travel_journal/src/components/location_data.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        id: 1,
+        coverImg: "../src/assets/hawaii.png",
+        state: "Hawaii",
+        mapLink: "https://goo.gl/maps/AUXRSh6xCYKBTLDj7",
+        title: "Aulani, Disney Resort",
+        dates: "7 June, 2022 - 12 June, 2022",
+        description: `"Aulani, a Disney Resort & Spa, is a beachside 
+        resort hotel at the Ko Olina Resort in Kapolei, Hawaii on the 
+        island of Oahu. Affiliated with the Disney Vacation Club, 
+        Aulani was the third Disney "stand-alone" hotel."`
+    },
+    {
+        id: 2,
+        coverImg: "../src/assets/myrtle_beach.png",
+        state: "SOUTH CAROLINA",
+        mapLink: "https://goo.gl/maps/62Nv6FNpCoPt5q3t5",
+        title: "Myrtle Beach",
+        dates: "20 June, 2022 - 25 June, 2022",
+        description: `"Myrtle Beach, is the hub of the Grand Strand, 
+        a 60-mile string of beaches. Its know for its beachfront boardwalk 
+        arcades, souvenir stands and restaurants, as well as the old-fashioned 
+        Family Kingdom amusement park and the SkyWheel, one of the country's 
+        tallest Ferris wheels."`
+    },
+    {
+        id: 3,
+        coverImg: "../src/assets/chattanooga.png",
+        state: "TENNESSEE",
+        mapLink: "https://goo.gl/maps/SxBsx3vfzzP5vpJo7",
+        title: "Chattanooga",
+        dates: "5 May, 2022 - 12 May, 2022",
+        description: `"Chattanooga, a city in southeastern Tennessee, is set 
+        along the Tennessee River in the foothills of the Appalachian Mountains. 
+        Its trolleylike Incline Railway scales steep Lookout Mountain before 
+        reaching Ruby Falls waterfall and Rock City."`
+    },
+
+]
