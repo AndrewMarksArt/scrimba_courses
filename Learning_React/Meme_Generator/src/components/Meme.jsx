7940c23c382c5c186ba9623fbfa3b8169b14707d
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-07 17:41:24 -0700

    added memeData which holds the data for the 100 memes being used by the meme generator. created an onClick event listener so when the get meme button is clicked it grabs the url for a random meme from memeData. currently just logs the ulr to console, need to make it so it show the image.

--- Learning_React/Meme_Generator/src/components/Meme.jsx
+++ Learning_React/Meme_Generator/src/components/Meme.jsx
@@ -1,13 +1,32 @@
 import "../css/main.css"
+import memeData from "./memeData"
 
 export default function Meme() {
+    function handleClick () {
+        const meme = memeData.data.memes[Math.floor(Math.random() * memeData.data.memes.length)]
+        console.log(meme.url)
+        return meme
+    }
     return (
         <main>
-            <form className="form">
-                <input type="text" className="form--input" placeholder="Top text" />
-                <input type="text" className="form--input" placeholder="Bottom text" />
-                <button className="form--button">Get a new meme image 🖼️</button>
-            </form>
+            <div className="form">
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Top text" 
+                />
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Bottom text" 
+                />
+                <button 
+                    className="form--button" 
+                    onClick={handleClick}
+                >
+                    Get a new meme image 🖼️
+                </button>
+            </div>
         </main>
     )
 }
