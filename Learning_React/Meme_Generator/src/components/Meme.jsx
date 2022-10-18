7b8ac35494ac3ff9a56c96f6246321c61e414632
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-10-18 10:36:07 -0700

    updated meme image and text styles. added input text to state and dynamically display top and bottom text as the user types in the input fields.

--- Learning_React/Meme_Generator/src/components/Meme.jsx
+++ Learning_React/Meme_Generator/src/components/Meme.jsx
@@ -21,6 +21,15 @@ export default function Meme() {
         }))
         
     }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
@@ -28,11 +37,17 @@ export default function Meme() {
                     type="text" 
                     className="form--input" 
                     placeholder="Top text" 
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text" 
                     className="form--input" 
                     placeholder="Bottom text" 
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="form--button" 
@@ -41,8 +56,10 @@ export default function Meme() {
                     Get a new meme image 🖼️
                 </button>
             </div>
-            <div>
-                <img src={meme.randomImage} className="memeImg"/>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
